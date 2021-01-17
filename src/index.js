7ef965bb7c794bd0ce6b18de00dc56b3f81c5d05
ioanae18 <ioanae18@yahoo.com>
2021-01-17 17:50:58 +0200

    some changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { findByLabelText } from '@testing-library/react';
+// import { findByLabelText } from '@testing-library/react';
 
 ReactDOM.render(
   <React.StrictMode>
